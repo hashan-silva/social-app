d93887b24eefb5bd3e44aa8c6537283cf75d4a47
hashan.silva <hashan.silva@gmail.com>
2024-11-21 20:35:45 +0100

    SOCAPP:crud operations

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,16 +14,18 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({ activities, selectActivity, selectedActivity, cancelSelectActivity,
-    editMode, openForm, closeForm, createOrEdit, deleteActivity }: Props) {
+    editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList activities={activities}
-                 selectActivity={selectActivity}
-                 deleteActivity={deleteActivity} />
+                    selectActivity={selectActivity}
+                    deleteActivity={deleteActivity}
+                    submitting={submitting} />
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity &&
@@ -37,6 +39,7 @@ export default function ActivityDashboard({ activities, selectActivity, selected
                         closeForm={closeForm}
                         activity={selectedActivity}
                         createOrEdit={createOrEdit}
+                        submitting={submitting}
                     />}
             </Grid.Column>
         </Grid>
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,3 +1,4 @@
+import { SyntheticEvent, useState } from "react";
 import { Activity } from "../../../app/model/activity";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 
@@ -5,9 +6,18 @@ interface Props {
     activities: Activity[];
     selectActivity: (id: string) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityList({ activities, selectActivity, deleteActivity }: Props) {
+export default function ActivityList({ activities, selectActivity, deleteActivity, submitting }: Props) {
+    const [target, setTarget] = useState('');
+
+
+    function handleActivityDelete(event: SyntheticEvent<HTMLButtonElement>, id: string): void {
+        setTarget(event.currentTarget.name);
+        deleteActivity(id);
+    }
+
     return (
         <Segment>
             <Item.Group divided>
@@ -21,9 +31,14 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button onClick={() => selectActivity(activity.id)} floated="right" content="View" color="blue"/>
-                                <Button onClick={() => deleteActivity(activity.id)} floated="right" content="Delete" color="red"/>
-                                <Label basic content={activity.category}/>
+                                <Button onClick={() => selectActivity(activity.id)} floated="right" content="View" color="blue" />
+                                <Button
+                                    name={activity.id}
+                                    loading={submitting && target === activity.id}                                    onClick={(event) => handleActivityDelete(event, activity.id)}
+                                    floated="right"
+                                    content="Delete"
+                                    color="red" />
+                                <Label basic content={activity.category} />
                             </Item.Extra>
                         </Item.Content>
                     </Item>
